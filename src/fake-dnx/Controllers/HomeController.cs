ce9012e851ef3377ff02d7a6537a80da0aa5cc3a
William Holroyd <wholroyd@gmail.com>
2015-07-28 22:36:19 -0400

    Initial commit

---
+++ src/fake-dnx/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace fake_aspnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Fake App";
+
+            return View();
+        }
+    }
+}
